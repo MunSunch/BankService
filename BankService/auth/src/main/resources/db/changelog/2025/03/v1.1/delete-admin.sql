974661722111691275453e8d1f18db665d1e9919
MunSun <msunchalyaev@gmail.com>
2025-03-18 18:01:37 +0400

    add migrations user with role=ADMIN

---
+++ BankService/auth/src/main/resources/db/changelog/2025/03/v1.1/delete-admin.sql
@@ -0,0 +1,1 @@
+DELETE FROM auth.users WHERE username = 'admin' AND password = 'admin' AND role = 'ROLE_ADMIN';
---
+++ BankService/auth/src/main/resources/db/changelog/2025/03/v1.1/insert-admin.sql
@@ -0,0 +1,1 @@
+INSERT INTO auth.users (username, password, role) VALUES ('admin', 'admin', 'ROLE_ADMIN');
